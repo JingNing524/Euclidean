18e8baa391cfbc5cf57b07990d26d382270057c2
Lynn <jning000000524@gmail>
2025-04-24 14:41:09 +0100

    Initial finish  refactor the code

--- euclidean.py
+++ euclidean.py
@@ -9,24 +9,91 @@ class EuclideanAlgorithm:
     
     def __init__(self, a, b):
         #two positive integers stored as private attributes _a and _b
-        self._a=a
-        self._b=b
+        
+        if not self._is_positive_integer(a) or not self._is_positive_integer(b):
+            self._a= None
+            self._b= None
+            self._valid= False
+        else:
+            self._a=a
+            self._b=b
+            self._valid= True
+        self._gcd= None
+        
+    def _is_positive_integer(self,n):
+        return type(n) ==int and n>0
     
-    def get_gcd(self):
+    def is_valid(self):
+        return self.valid
+    
+    def caculate_gcd(self):
+        if not self._valid:
+            return None
         
         A=self._a   #make it a variable
         B=self._b   #make it a variable
-        
+     
         while B!=0:  
             R=A%B   #use modolus to find the remainder
             A=B   #the new A is the old B
             B=R   #the new B is the remainder
-        return A   #when B=0, A holds the GCD
+            self._gcd=A
+        return self._gcd   #when B=0, A holds the GCD
 
-input_a=270
-input_b=192       
+        
+    
+    def get_gcd(self):
+        if not self.valid:
+           return None
+        if self._gcd is None:
+           self.caculate_gcd()
+           return self._gcd
+       
+def get_valid_integer_input(prompt):
+    while True:
+        user_input=input(prompt)
+        if not user_input or '' in user_input:
+            print('Invalid input: Please enter a single number without spaces.')
+            continue
+           
+        is_digit_string=True
+        for char in user_input:
+            if not ('0' <= char <= '9'):
+                is_digit_string=False
+                break
+            
+            if is_digit_string:
+                number= int(user_input)
+                if number>0:
+                    return number
+                else:
+                    print('invalid input: Please enter a positive integer.')
+            else:
+                print('Invalid input: Please enter an integer.')
+                
+if __name__ =='__main__':
+    print('Welcome to the Euclidean Algorithm GCD caculator!')
+        
+    while True:
+        A= get_valid_integer_input('Enter your first positive integer number: ')
+        if A is None:
+            continue
+        B= get_valid_integer_input('Enter your second positive integer number: ')
+        if B is None:
+            continue
+            
+        solve=EuclideanAlgorithm(A, B)
+        if solve.is_valid():
+            gcd_result=solve.get_gcd()
+            print(f'GCD({A},{B})={gcd_result}')
+            break
+        else:
+            print("Error: Invalid input provided. Please try again.")
+        
+                
+                
+                   
+         
+        
 
-
-example=EuclideanAlgorithm(input_a, input_b)   
-gcd_result=example.get_gcd()
-print(f"GCD({input_a}, {input_b})={gcd_result}")     
+  
